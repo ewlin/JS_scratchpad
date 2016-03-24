529f17aa7a6b30ba1d763c9a83a76efaf58c0fb8
ewlin <ericwilliamlin@gmail.com>
2016-03-24 15:19:01 -0400

    re-format

--- parseMolecule.js
+++ parseMolecule.js
@@ -13,8 +13,8 @@ var parseMolecule = function(formula) {
     //preprocess formula string and strip out the different braces/brackets and whitespace
     var updatedFormula = updatedFormula ||
                          formula.replace(/\]|\}/g, ')')
-                        .replace(/\{|\[/g, '(')
-                        .replace(/\s/g, ''), 
+                                .replace(/\{|\[/g, '(')
+                                .replace(/\s/g, ''), 
 
     //match all of the inner-most parenthesis-grouped elements e.g. (H2OCl)3
         nested = updatedFormula.match(/\(\w*\)\d*/g),
@@ -26,15 +26,15 @@ var parseMolecule = function(formula) {
     //logic
     if (nested) { //e.g. if groupings like "(H2OCl)3", "(SO4)2" are in molecule input
         nested.forEach(function(innerMol) { //A: "(H2OCl)3" B: "(SO4)2"
-            var flattened = "",
+            var flattened = '',
                 innerMolArr = (innerMol.slice(1)).split(')'), // "(SO4)2" --> "SO4)2" --> ["SO4","2"]
                 multiple = Number(innerMolArr[1]) || 1; // ["S04","2"] -> 2
-
-            moleculeToElementsArray(innerMolArr[0]) // ["SO4","2"][0]--> "SO4" --> .... --> ["S","O4"].forEach(fn)...
-                .forEach(function(ele) {
-                    var numOf = Number(ele.replace(nums, function(digits) { return ',' + digits }).split(',')[1]) || 1;
-                    flattened += (ele.match(atom) + (numOf * multiple));
-                });
+            
+            // ["SO4","2"][0]--> "SO4" --> .... --> ["S","O4"].forEach(fn)...
+            moleculeToElementsArray(innerMolArr[0]).forEach(function(ele) {
+                var numOf = Number(ele.replace(nums, function(digits) { return ',' + digits }).split(',')[1]) || 1;
+                flattened += (ele.match(atom) + (numOf * multiple));
+            });
 
             updatedFormula = updatedFormula.replace(innerMol, flattened); //replace (H2OCl)3 with H6O3Cl3
         });
@@ -45,15 +45,14 @@ var parseMolecule = function(formula) {
         /*takes something like "H6O3Cl3", splits into an array of elements--i.e. ["H6","O3","Cl3"], and reduces array 
         to returns an object with element counts for each /molecule--e.g. {H: 6, O: 3, Cl: 3}
         */
-        return moleculeToElementsArray(updatedFormula)
-                .reduce(function(elementCount, element) {
-                    var numOf = Number(element.match(nums)) || 1;
-                    if (elementCount.hasOwnProperty(element.match(atom)))
-                        elementCount[element.match(atom)] += numOf;
-                    else
-                        elementCount[element.match(atom)] = numOf;
-                    return elementCount;
-                }, {});
+        return moleculeToElementsArray(updatedFormula).reduce(function(elementCount, element) {
+                   var numOf = Number(element.match(nums)) || 1;
+                   if (elementCount.hasOwnProperty(element.match(atom)))
+                       elementCount[element.match(atom)] += numOf;
+                   else
+                       elementCount[element.match(atom)] = numOf;
+                   return elementCount;
+               }, {});
     }
 
     //helper function
