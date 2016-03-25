be61103c81a68254555b56f4f2c953fdb7dceb28
ewlin <ericwilliamlin@gmail.com>
2016-03-24 23:54:22 -0400

    undoRedo

--- undoRedo.js
+++ undoRedo.js
@@ -5,67 +5,67 @@ function undoRedo(object) {
     var allowUndo = true; 
     
 	return {
-      
-		set: function(key, value) {
-            if (!allowUndo) {
-                allowUndo = true, _undos = [], _redos = []; 
-            }
-            if (object[key]) {
-                _undos.push({key: key, oldValue: object[key], newValue: value, op: 'updated'});  
-            } else {
-                _undos.push({key: key, value: value, op: 'added'});  
-            }
-            object[key] = value;   
-        },
+    
+    set: function(key, value) {
+        if (!allowUndo) {
+            allowUndo = true, _undos = [], _redos = []; 
+        }
+        if (object[key]) {
+            _undos.push({key: key, oldValue: object[key], newValue: value, op: 'updated'});  
+        } else {
+            _undos.push({key: key, value: value, op: 'added'});  
+        }
+        object[key] = value;   
+    },
         
-		get: function(key) {
-            return object[key];
-        },
+    get: function(key) {
+        return object[key];
+    },
         
-		del: function(key) { 
-            if (!allowUndo) {
-                allowUndo = true, _undos = [], _redos = []; 
+    del: function(key) { 
+        if (!allowUndo) {
+            allowUndo = true, _undos = [], _redos = []; 
+        }
+        _undos.push({key: key, value: object[key], op: 'deleted'});  
+        delete object[key]; 
+    },
+        
+	undo: function() {
+        var undoNums = _undos.length; 
+        
+        if (allowUndo) allowUndo = false; 
+        if (undoNums) {
+            if (_undos[undoNums-1]['op'] === 'added') {
+                delete object[_undos[undoNums-1]['key']];
+            } else if (_undos[undoNums-1]['op'] === 'deleted') {
+                object[_undos[undoNums-1]['key']] = _undos[undoNums-1]['value']; 
+            } else if (_undos[undoNums-1]['op'] === 'updated') {
+                object[_undos[undoNums-1]['key']] = _undos[undoNums-1]['oldValue']; 
             }
-            _undos.push({key: key, value: object[key], op: 'deleted'});  
-            delete object[key]; 
-        },
-        
-		undo: function() {
-            var undoNums = _undos.length; 
+            _redos.push(_undos.pop()); 
             
-            if (allowUndo) allowUndo = false; 
-            if (undoNums) {
-                if (_undos[undoNums-1]['op'] === 'added') {
-                    delete object[_undos[undoNums-1]['key']];
-                } else if (_undos[undoNums-1]['op'] === 'deleted') {
-                    object[_undos[undoNums-1]['key']] = _undos[undoNums-1]['value']; 
-                } else if (_undos[undoNums-1]['op'] === 'updated') {
-                    object[_undos[undoNums-1]['key']] = _undos[undoNums-1]['oldValue']; 
-                }
-                _redos.push(_undos.pop()); 
-                
-            } else {
-                throw new Error('nothing to undo'); 
-            }
-        },
+        } else {
+            throw new Error('nothing to undo'); 
+        }
+    },
                              
-        redo: function() {
-            var redoNums = _redos.length; 
+    redo: function() {
+        var redoNums = _redos.length; 
 
-            if (redoNums) {
-                if (_redos[redoNums-1]['op'] === 'added') {
-                    object[_redos[redoNums-1]['key']] = _redos[redoNums-1]['value'];
-                } else if (_redos[redoNums-1]['op'] === 'deleted') {
-                    delete object[_redos[redoNums-1]['key']]; 
-                } else if (_redos[redoNums-1]['op'] === 'updated') {
-                    object[_redos[redoNums-1]['key']] = _redos[redoNums-1]['newValue']; 
-                }
-                _undos.push(_redos.pop()); 
-
-            } else {
-                throw new Error('nothing to redo'); 
+        if (redoNums) {
+            if (_redos[redoNums-1]['op'] === 'added') {
+                object[_redos[redoNums-1]['key']] = _redos[redoNums-1]['value'];
+            } else if (_redos[redoNums-1]['op'] === 'deleted') {
+                delete object[_redos[redoNums-1]['key']]; 
+            } else if (_redos[redoNums-1]['op'] === 'updated') {
+                object[_redos[redoNums-1]['key']] = _redos[redoNums-1]['newValue']; 
             }
-        }               
+            _undos.push(_redos.pop()); 
+        } else {
+            throw new Error('nothing to redo'); 
+        }
+    } 
+        
     }	
 }
 
